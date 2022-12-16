982a1609de6517bf88a1d5d27a27164058796a43
Dan Prince <1266011+danprince@users.noreply.github.com>
2022-12-16 23:28:03 +0100

    adds additional keybindings

--- src/input.zig
+++ src/input.zig
@@ -3,10 +3,25 @@ const engine = @import("engine.zig");
 const Action = actions.Action;
 
 const keys = struct {
+    // Arrow keys
     pub const left_arrow = 37;
     pub const up_arrow = 38;
     pub const right_arrow = 39;
     pub const down_arrow = 40;
+
+    // Vi keys
+    pub const h = 72;
+    pub const j = 74;
+    pub const k = 75;
+    pub const l = 76;
+    pub const y = 89;
+    pub const u = 85;
+    pub const b = 66;
+    pub const n = 78;
+
+    // Other keys
+    pub const space = 32;
+    pub const period = 190;
 };
 
 pub const InputEventType = enum {
@@ -27,10 +42,15 @@ pub const EventHandler = struct {
     fn dispatch(_: *Self, event: InputEvent) ?Action {
         return switch (event) {
             .keydown => |key| switch (key) {
-                keys.left_arrow => actions.bump(-1, 0),
-                keys.right_arrow => actions.bump(1, 0),
-                keys.up_arrow => actions.bump(0, -1),
-                keys.down_arrow => actions.bump(0, 1),
+                keys.space, keys.period => actions.wait(),
+                keys.h, keys.left_arrow => actions.bump(-1, 0),
+                keys.l, keys.right_arrow => actions.bump(1, 0),
+                keys.k, keys.up_arrow => actions.bump(0, -1),
+                keys.j, keys.down_arrow => actions.bump(0, 1),
+                keys.y => actions.bump(-1, -1),
+                keys.u => actions.bump(1, -1),
+                keys.b => actions.bump(-1, 1),
+                keys.n => actions.bump(1, 1),
                 else => null,
             },
             else => null,
