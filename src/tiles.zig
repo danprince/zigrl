914a86d3b648ffd1e9f97745038ac4a4b361c523
Dan Prince <1266011+danprince@users.noreply.github.com>
2022-12-17 21:57:22 +0100

    improve tile appearances

--- src/tiles.zig
+++ src/tiles.zig
@@ -9,20 +9,20 @@ pub const shroud = Graphic{ .ch = ' ', .fg = rgb(255, 255, 255), .bg = rgb(0, 0,
 pub const floor = Tile{
     .walkable = true,
     .transparent = true,
-    .dark = .{ .ch = ' ', .fg = rgb(255, 255, 255), .bg = rgb(50, 50, 150) },
-    .light = .{ .ch = ' ', .fg = rgb(255, 255, 255), .bg = rgb(200, 180, 50) },
+    .dark = .{ .ch = 0x9, .fg = rgb(20, 20, 20), .bg = null },
+    .light = .{ .ch = 0xA, .fg = rgb(50, 50, 60), .bg = null },
 };
 
 pub const wall = Tile{
     .walkable = false,
     .transparent = false,
-    .dark = .{ .ch = ' ', .fg = rgb(255, 255, 255), .bg = rgb(0, 0, 100) },
-    .light = .{ .ch = ' ', .fg = rgb(255, 255, 255), .bg = rgb(130, 110, 50) },
+    .dark = .{ .ch = 0x7, .fg = rgb(50, 50, 60), .bg = null },
+    .light = .{ .ch = 0x8, .fg = rgb(100, 100, 120), .bg = null },
 };
 
 pub const down_stairs = Tile{
     .walkable = true,
     .transparent = true,
-    .dark = .{ .ch = '>', .fg = rgb(0, 0, 100), .bg = rgb(50, 50, 150) },
-    .light = .{ .ch = '>', .fg = rgb(255, 255, 255), .bg = rgb(200, 180, 50) },
+    .dark = .{ .ch = '>', .fg = rgb(0, 0, 100), .bg = null },
+    .light = .{ .ch = '>', .fg = rgb(255, 255, 255), .bg = null },
 };
